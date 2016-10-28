7fbfcbbae8feae6873af92c0194e086481598ae7
yamahayeuhonda <yamahayeuhonda@gmail.com>
2016-10-28 17:21:06 +0700

    In hàng đếm
    bổ sung vòng for để in ra số lần đếm

--- src/Core/TheLibraries.java
+++ src/Core/TheLibraries.java
@@ -4,7 +4,10 @@ public class TheLibraries {
 
 	public static void main(String[] args) {
 		System.out.println("fuck GITHUB");
-
+		int i;
+		for(i=0;i<10;i++){
+			System.out.println("Count"+i+" : "+i);
+		}
 	}
 
 }
