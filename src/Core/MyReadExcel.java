d8798f1726c6d1274b9594797966b7581306d5e6
yamahayeuhonda <yamahayeuhonda@gmail.com>
2016-11-01 17:12:37 +0700

    Bổ sung các điều kiện
    bổ sung việc kiểm tra file có tồn tại hay không trước khi sử lý, đưa ra
    thông báo rõ ràng hơn nếu file không tồn tại

--- src/Core/MyReadExcel.java
+++ src/Core/MyReadExcel.java
@@ -3,58 +3,47 @@ package Core;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 
-import javax.swing.text.html.HTMLDocument.Iterator;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-
-import com.sun.javafx.scene.control.skin.VirtualFlow.ArrayLinkedList;
-import com.sun.xml.internal.bind.v2.runtime.reflect.Lister.IDREFSIterator;
-
-import javafx.animation.Interpolator;
-import java.util.*;
 
 public class MyReadExcel {
-	
-//	private File f;
-	private static final String FILE_PATCH = "baocaoluuluongxequatram_10282016203253.xls";
-	private POIFSFileSystem poifile;
+
+	// private File f;
+	private static final String FILE_PATCH = "baocaoluuluongxequatram_10282016203253.xlsx";
+	// private NPOIFSFileSystem poifile;
 	private FileInputStream inputstream;
-	private HSSFWorkbook workbook;
-	private HSSFSheet sheet;
-	
-	
-	private void readExcel(File f){
-		
-		try {
-//			f= new File(FILE_PATCH);
-			inputstream = new FileInputStream(f);
-			poifile = new POIFSFileSystem(inputstream);
-			workbook = new HSSFWorkbook(poifile);
-			sheet = workbook.getSheetAt(1);
-			System.out.println(sheet.getSheetName());
-			System.out.println("tong so row: "+ sheet.getLastRowNum());
+	private XSSFWorkbook workbook;
+	private XSSFSheet sheet;
+
+	private void readExcel(File f) {
+
+		if (f.exists()) {
+			try {
+				// i tried to use XSSFWorkbook(file) but error happening >
+				// using filestream
+				inputstream = new FileInputStream(f);
+				workbook = new XSSFWorkbook(inputstream);
+				sheet = workbook.getSheetAt(0);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println(FILE_PATCH);
+			}
+			/*System.out.println(sheet.getSheetName());
+			System.out.println("tong so row: " + sheet.getLastRowNum());
+			*/
 			
-			/*for(Iterator rit = (Iterator)sheet.rowIterator(); rit.next();){
-				System.out.println(sheet.rowIterator());
-				
-			}*/
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.println(FILE_PATCH);
+		} else {
+			System.out.println("ERROR: File not found.");
 		}
-		
+
 	}
 
 	public static void main(String[] args) {
-		
-		File f = new File(FILE_PATCH);		
+
+		File f = new File(FILE_PATCH);
 		MyReadExcel me = new MyReadExcel();
 		me.readExcel(f);
 	}
