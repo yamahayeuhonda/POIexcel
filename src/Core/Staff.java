0a85e8c77410192b513576b98b25d5127448f506
yamahayeuhonda <yamahayeuhonda@gmail.com>
2016-11-01 17:13:42 +0700

    Tạo đối tượng Staff - nhân viên
    Tạo đối tượng Staff nhằm lưu trữ các thuộc tính, thuận tiện cho việc lấy
    dữ liệu trong các cell trên file excel

---
+++ src/Core/Staff.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package Core;
+
+/**
+ * @author neo
+ *
+ */
+public class Staff {
+	
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String sex;
+	private String address;
+	private double phoneNumber;
+	private double identificationnumber;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public double getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(double phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public double getIdentificationnumber() {
+		return identificationnumber;
+	}
+	public void setIdentificationnumber(double identificationnumber) {
+		this.identificationnumber = identificationnumber;
+	}
+	
+
+}
